168a9c65a7a21b217f2e8a1f40ab2cac8943bbd8
Vinoth <rvinoth@std.foc.sab.ac.lk>
2025-03-04 00:25:33 +0530

    Add Testimonials section for the website

--- edusity/src/App.jsx
+++ edusity/src/App.jsx
@@ -5,6 +5,7 @@ import Programs from './Components/Programs/Programs'
 import Title from './Components/Title/Title'
 import About from './Components/About/About'
 import Campus from './Components/Campus/Campus'
+import Testimonials from './Components/Testimonials/Testimonials'
 
 const App = () => {
   return (
@@ -17,6 +18,8 @@ const App = () => {
         <About/>
         <Title subTitle='Gallery' title='Campus Photos'/>
         <Campus/>
+        <Title subTitle='Testimonials' title='What Student Says'/>
+        <Testimonials/>
       </div>
     </div>
   )
