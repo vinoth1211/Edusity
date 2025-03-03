f3e0dc3369a4d331319a1d2d6fa6162e1a163afc
Vinoth <rvinoth@std.foc.sab.ac.lk>
2025-03-03 21:19:31 +0530

    Add Home section for website

--- edusity/src/App.jsx
+++ edusity/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './Components/Navigation Bar/Navbar'
+import Hero from './Components/Hero/Hero'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <Hero/>
     </div>
   )
 }
