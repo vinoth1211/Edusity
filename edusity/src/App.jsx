853c4d92160aecb8f39aeb0dd41b3faa2e1c34f7
Vinoth <rvinoth@std.foc.sab.ac.lk>
2025-03-04 23:27:08 +0530

    Add footer and completed the website

--- edusity/src/App.jsx
+++ edusity/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Components/Navigation Bar/Navbar'
 import Hero from './Components/Hero/Hero'
 import Programs from './Components/Programs/Programs'
@@ -7,8 +7,13 @@ import About from './Components/About/About'
 import Campus from './Components/Campus/Campus'
 import Testimonials from './Components/Testimonials/Testimonials'
 import Contact from './Components/Contact/Contact'
+import Footer from './Components/Footer/Footer'
+import VideoPlayer from './Components/VideoPlayer/VideoPlayer'
 
 const App = () => {
+
+  const [playState, setPlayState] = useState(false);
+
   return (
     <div>
       <Navbar/>
@@ -16,14 +21,16 @@ const App = () => {
       <div className="container">
         <Title subTitle='Our Program' title='What We Offer'/>
         <Programs/>
-        <About/>
+        <About setPlayState={setPlayState}/>
         <Title subTitle='Gallery' title='Campus Photos'/>
         <Campus/>
         <Title subTitle='Testimonials' title='What Student Says'/>
         <Testimonials/>
         <Title subTitle='Contact Us' title='Get in Touch'/>
         <Contact/>
+        <Footer/>
       </div>
+      <VideoPlayer playState={playState} setPlayState={setPlayState}/>
     </div>
   )
 }
